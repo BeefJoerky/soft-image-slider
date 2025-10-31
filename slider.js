b636c2c57c70fa5afe6b5f146fed9b317cae4e7f
Gustav Jörg <gustav.jorg@bahnhof.se>
2025-10-31 16:00:36 +0100

    Flera funktioner tillagda

--- slider.js
+++ slider.js
@@ -76,7 +76,7 @@ function getImageNode(index) {
 
 // Discrete as in whole image pixel sizes
 function calculatePositionInPixels() {
-    let unseenOffset = (curIndex * imSize + Math.max((curIndex-1) * gap, 0));    // Clamp with Math.min to remove the case for curIndex=0
+    let unseenOffset = (curIndex * imSize + Math.max((curIndex) * gap, 0));    // Clamp with Math.min to remove the case for curIndex=0
     let totalOffset = unseenOffset - offset;
 
     return totalOffset;
@@ -92,6 +92,7 @@ function recalculatePercentage() {
 
 
 function offsetSlider() {
+    console.log("Offsetting slider to: " + offset + "px")
     slider.style.transform = "translateX(" + offset + "px";
 }
 
@@ -111,11 +112,107 @@ function scrollByPixels(pixels) {
     let curX = calculatePositionInPixels();
     let newX = curX + pixels;
     newX = Math.max(newX, 0);
+    //let minX = totalSliderPixelSize - (imSize+gap) * numInView;
+    newX = Math.min(newX, totalSliderPixelSize);
+    console.log("Pixels: ", pixels, "NewX: ", newX)
 
     // Reinitialize images for simplicity
-    reinitImages(newX);
+    //reinitImages(newX);
 
-    offsetSlider();
+    // Check if an image should be moved
+    // Multiple images could be moved if pixels is big
+    let moveImages = checkUnseenImages(curX, newX);
+    console.log(moveImages)
+
+    
+    
+    //let first = moveImages[0];
+    //let last = moveImages[moveImages.length-1];
+    //console.log(last)
+    // If the entire new set of images is outside of current view
+    if (moveImages > numWithUnseen) {
+        
+        //let diff = first < 0 ? first : last - numWithUnseen + 1;
+        if (pixels > 0) {
+            curIndex += moveImages
+        }
+        else {
+            curIndex -= moveImages
+        }
+        curIndex = curIndex.clamp(0, totalElements-numWithUnseen);
+        console.log(curIndex)
+        
+        
+    }
+
+    if (moveImages == 0) {
+        offset = -newX % (imSize+gap)
+        offsetSlider();
+        return
+    }
+    
+    // Slide right
+    if (pixels < 0) {
+        console.log("Yes", moveImages)
+        for (i = 0; i <= moveImages; i++) {
+            if (curIndex == 0) {
+                break
+            }
+            switchLast();
+        }
+    }
+    // Slide left
+    else {
+        for (i = 0; i <= moveImages; i++) {
+            if (curIndex == totalElements - numWithUnseen) {
+                break
+            }
+            switchFirst();
+        }
+    }
+    
+    //
+    
+}
+
+
+function checkUnseenImages(curX, newX) {
+    let curOffset = offset;
+    let newDiff = newX - curX;
+    let tentativeOffset = newDiff;
+    
+    const unseenUpperLimit = (imSize + gap) * 2;
+    const unseenLowerLimit = (imSize + gap) * 1;
+    console.log(tentativeOffset, newX);
+
+    // If the move action would render images outside the rendered zone
+    
+    // If at the beginning; exception case, where additional images should be rendered on the side
+    // If trying to slide to slide past the beginning
+    if (curIndex == 0 && newDiff > 0) {
+        // No-op, only offset
+        return 0;
+    }
+
+    // TODO: handle right-most case
+    
+    if (tentativeOffset < -unseenUpperLimit) {
+        let oldNumLeft = Math.floor(curX / (imSize+gap));
+        let newNumLeft = Math.floor((newX-gap) / (imSize+gap));
+        console.log(newNumLeft-curIndex, newNumLeft)
+        return newNumLeft-curIndex;
+        //return [...Array(newNumLeft-curIndex-1).keys()];
+    }
+    else if (tentativeOffset > -unseenLowerLimit) {
+        
+        let oldNumRight = totalElements - Math.floor(curX / (imSize + gap)) - numWithUnseen;
+        let newNumRight = Math.floor((newX + (imSize+gap)*numWithUnseen) / (imSize+gap));
+        return numWithUnseen - (newNumRight-curIndex)
+        //return [...Array(numWithUnseen - (newNumRight-curIndex) +1).keys()].map(i => i + newNumRight-curIndex-1);
+    }
+    
+    return 0;
+    
 }
 
 
@@ -209,7 +306,7 @@ function initSlider() {
 
 // The slider size in this regard "begins" at the middle of the slider, at curIndex=0, and ends at the middle of the slider when curIndex = (totalElements-)
 function calculateSliderPixelSize() {
-    let totSize = imSize * totalElements + gap * (totalElements-2);
+    let totSize = imSize * totalElements + gap * (totalElements-2) + gap;
 
     //  let halfViewSize = sliderContainerSize/2    Since this is subtracted from totSize twice, ignore this and subtract sliderContainerSize directly
 
@@ -227,28 +324,38 @@ function toggleVisibility(imIndex, isHidden) {
 }
 
 
-function reinitImages(initX) {
+function reinitImages() { //(initX) {
+    /*
     let imSizesLeft = Math.floor(initX / (imSize+gap));
     let smallOffset = -initX % (imSize + gap);
-    console.log(smallOffset);
+    console.log(initX)
+    console.log(imSizesLeft);
 
 
     curIndex = imSizesLeft-1;
+    console.log(curIndex, imSizesLeft)
     
     // The left-most edge case
     if (imSizesLeft < 2) {
         offset = smallOffset - imSizesLeft * (imSize+gap)
         curIndex = 0;
-        console.log("First: " + offset);
+        //console.log("First: " + offset);
+    }
+    else if (imSizesLeft > totalElements-numWithUnseen) {
+        let prevOffset = totalSliderPixelSize
+        offset = smallOffset-(totalElements-imSizesLeft-numInView+1)*(imSize+gap)
+        //offset = calculatePositionInPixels() - initX;
+        curIndex = totalElements-numWithUnseen;
+        console.log(curIndex, imSizesLeft, smallOffset, offset)
     }
     // If there are hidden images to the left, the most common scenario
     else {
         offset = smallOffset - imSize - gap;
-        console.log("Second: " + offset);
+        //console.log("Second: " + offset);
     }
 
-    console.log(curIndex);
-
+    //console.log(curIndex);
+    */
     for (i = 0; i < numWithUnseen; i++) {
         getImageNode(i).text = loadData(curIndex + i);
     }
